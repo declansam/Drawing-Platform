73d6b7b223b9b1d04158ddc8f3bf3655fbb6ad71
Samyam Lamichhane <71516511+declansam@users.noreply.github.com>
2022-12-10 21:32:37 +0400

    Create keyboard_keys.js

---
+++ p5.js/keyboard_keys.js
@@ -0,0 +1,13 @@
+
+
+// Set boolean to true when mouse is clicked
+function mousePressed()
+{ 
+  isClicked = true;
+}
+
+
+// Set boolean to false when mouse is released
+function mouseReleased() {
+  isClicked = false;
+}
